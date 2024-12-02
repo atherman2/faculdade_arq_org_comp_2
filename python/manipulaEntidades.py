4486d1431ee13d16623b5694bdafcede0991bdd7
atherman2 <alexandre.t.herman@gmail.com>
2024-12-02 16:23:21 -0300

    feito primeiro caso lerPalavra
    caso em que foi encontrada uma cache com linha de tag correspondente e estado MESIF igual a Forward

--- python/manipulaEntidades.py
+++ python/manipulaEntidades.py
@@ -1,40 +1,71 @@
 from entidades import *
 
-def lerPalavra(conjuntoProcCaches: ConjuntoProcessadoresCaches, memoriaPrincipal: MemoriaPrincipal, endereco: int, indiceProcCache: int):
+def lerPalavra(conjuntoProcCaches: ConjuntoProcessadoresCaches, memoriaPrincipal: MemoriaPrincipal, endereco: int, indiceProcCacheRequisitante: int):
 
-    palavrasPorLinha = conjuntoProcCaches.procCaches[indiceProcCache].palavrasPorLinha
+    palavrasPorLinha = conjuntoProcCaches.procCaches[indiceProcCacheRequisitante].palavrasPorLinha
     tag = endereco//palavrasPorLinha
     indicePalavra = endereco % palavrasPorLinha
 
-    procCacheAlvo = conjuntoProcCaches.procCaches[indiceProcCache]
-    linhaAlvo = buscaLinhaCache(procCacheAlvo, tag)
-    encontrouLinhaValida = False
-    if linhaAlvo != None:
+    procCacheRequisitante = conjuntoProcCaches.procCaches[indiceProcCacheRequisitante]
+    linhaComTagCorrespondente = buscaLinhaCache(procCacheRequisitante, tag)
 
-        if linhaAlvo.tag != EstadoMesif.INVALID:
+    if linhaComTagCorrespondente != None:
+
+        if linhaComTagCorrespondente.tag != EstadoMesif.INVALID:
 
             # TODO: Exibir Cache Hit
             encontrouLinhaValida = True
-            return linhaAlvo.palavras[indicePalavra]
+            return linhaComTagCorrespondente.palavras[indicePalavra]
     
-    if not encontrouLinhaValida:
+    # TODO: Exibir Cache Miss
+    indiceProcCacheAtual = 0
+    encontrouLinhaForward = False
+    while (not encontrouLinhaForward) and (indiceProcCacheAtual < conjuntoProcCaches.quantidadeProcCaches):
 
-        # TODO: Exibir Cache Miss
-        indiceProcCacheAtual = 0
-        encontrouLinhaForward = False
-        while (not encontrouLinhaForward) and (indiceProcCacheAtual < conjuntoProcCaches.quantidadeProcCaches):
-            if indiceProcCacheAtual != indiceProcCache:
-                linhaAlvoProcCacheAtual: LinhaCache = buscaLinhaCache(conjuntoProcCaches.procCaches[indiceProcCacheAtual])
-                if linhaAlvoProcCacheAtual.estadoMesif == EstadoMesif.FORWARD:
-                    encontrouLinhaForward = True
+        if indiceProcCacheAtual != indiceProcCacheRequisitante:
+
+            linhaAlvoProcCacheAtual: LinhaCache = buscaLinhaCache(conjuntoProcCaches.procCaches[indiceProcCacheAtual])
+            if linhaAlvoProcCacheAtual.estadoMesif == EstadoMesif.FORWARD:
+
+                encontrouLinhaForward = True
+                linhaForward = linhaAlvoProcCacheAtual
+    
+    if encontrouLinhaForward:
+
+        if linhaComTagCorrespondente == None:
+
+            copiaLinha(linhaForward, procCacheRequisitante.linhas[procCacheRequisitante.indiceSubstituicao], procCacheRequisitante.palavrasPorLinha)
+            procCacheRequisitante.linhas[procCacheRequisitante.indiceSubstituicao].estadoMesif = EstadoMesif.SHARED
+            
+            procCacheRequisitante.indiceSubstituicao += 1
+            if procCacheRequisitante.indiceSubstituicao == procCacheRequisitante.quantidadeDeLinhas:
+                
+                procCacheRequisitante.indiceSubstituicao = 0
 
 def buscaLinhaCache(procCache: ProcessadorCache, tag) -> LinhaCache:
 
     indiceLinhaCacheAtual = 0
     encontrou = False
     while (not encontrou) and (indiceLinhaCacheAtual < procCache.quantidadeDeLinhas):
+
         encontrou = (procCache.linhas[indiceLinhaCacheAtual].tag == tag)
         if not encontrou:
+        
             indiceLinhaCacheAtual += 1
     if encontrou:
+        
         return procCache.linhas[indiceLinhaCacheAtual]
+
+def copiaLinha(linhaFonte: LinhaCache, linhaDestino: LinhaCache, tamanhoLinha: int):
+
+    linhaDestino.tag = linhaFonte.tag
+    linhaDestino.sendoUsada = linhaFonte.sendoUsada
+
+    for indicePalvraAtual in range(tamanhoLinha):
+        
+        copiaPalavra(linhaFonte.palavras[indicePalvraAtual], linhaDestino.palavras[indicePalvraAtual])
+
+def copiaPalavra(palavraFonte: Palavra, palavraDestino: Palavra):
+
+    palavraDestino.conteudo = palavraFonte.conteudo
+    palavraDestino.sendoUsada =  palavraFonte.sendoUsada
